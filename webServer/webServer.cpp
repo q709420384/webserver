e0a1dd16ad04bff0d72e8c4afda4eda4080bd007
ljh <q709420384@gmail.com>
2018-05-19 18:40:28 +0800

    webserver

---
+++ webServer/webServer.cpp
@@ -0,0 +1,63 @@
+#include "task.h"
+#include "thread_pool.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+    	{
+		printf("usage : %s port\n", argv[0]);
+		return 1;
+    	}
+
+    	int sockfd, connfd;
+   	struct sockaddr_in servaddr, client;
+    	int port = atoi(argv[1]);  //获取端口
+    	//设置服务端的sockaddr_in
+    	servaddr.sin_family = AF_INET;
+    	servaddr.sin_port = htons(port);
+    	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);      //host to network long
+    	//创建socket
+    	sockfd = socket(AF_INET, SOCK_STREAM, 0);        //分配套接字
+    	if(sockfd < 0)
+    	{
+		printf("socket error\n");
+		return 1;
+    	}
+    	int reuse = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1)
+        {
+                return -1;
+        }
+
+    	//绑定
+    	int ret = bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    	if(ret < 0)
+    	{
+    		perror("bind ");
+		//printf("bind error\n");
+		return 1;
+    	}
+    	//监听
+    	ret = listen(sockfd, 10);
+    	if(ret < 0)
+    	{
+		printf("listen error\n");
+		return 1;
+    	}
+    	//创建线程池
+    	threadpool<task> pool(20);
+    	pool.start();  //线程池开始运行
+
+    	while(1)
+    	{
+    		//printf("new connection\n");
+		socklen_t len = sizeof(client); 
+		//接受连接
+		connfd = accept(sockfd, (struct sockaddr *)&client, &len);
+		printf("accept a new client: %s:%d\n",inet_ntoa(client.sin_addr),client.sin_port);
+		task *ta = new task(connfd);
+		//向线程池添加任务
+		pool.append_task(ta);
+    	}
+    	return 0;
+}
